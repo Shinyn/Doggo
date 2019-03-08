127fe0ff6e77d90f56feea001b2ef1a9f4e89bed
Shinyn <mattias.alm@hotmail.com>
2019-03-08 20:40:42 +0100

    onClick for mainActivity and minor translation adjustments

--- app/src/main/java/com/l/doggo/HomeActivity.java
+++ app/src/main/java/com/l/doggo/HomeActivity.java
@@ -3,26 +3,11 @@ package com.l.doggo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-
-
-
-
-
-
-
-
-
-
-
-        // if login is correct -> byt view till home
-
-
+        setContentView(R.layout.activity_home);
     }
 }
--- app/src/main/java/com/l/doggo/MainActivity.java
+++ app/src/main/java/com/l/doggo/MainActivity.java
@@ -1,13 +1,44 @@
 package com.l.doggo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Intent intent;
+    Intent intent2;
+    Intent intent3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        intent = new Intent(this, LoginActivity.class);
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+
+
+        intent2 = new Intent(this, CreateAccountActivity.class);
+        Button createAccountButton = findViewById(R.id.createAccountButton);
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent2);
+            }
+        });
+
+
+        // Behöver lägga till annonsknappen (när den view'n är klar)
+
     }
 }
