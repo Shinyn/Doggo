2a1f168bd35730d29b98a6b1cb3de5e249c1f9ab
Shinyn <mattias.alm@hotmail.com>
2019-03-28 13:54:32 +0100

    Drawer menu and more design

--- app/src/main/java/com/l/doggo/HomeActivity.java
+++ app/src/main/java/com/l/doggo/HomeActivity.java
@@ -1,8 +1,11 @@
 package com.l.doggo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,6 +14,9 @@ public class HomeActivity extends AppCompatActivity {
     Intent intent;
     Intent intent2;
     Intent intent3;
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,19 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navView);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        drawerLayout.closeDrawers();
+
+                        return true;
+                    }
+                });
 
 
 
