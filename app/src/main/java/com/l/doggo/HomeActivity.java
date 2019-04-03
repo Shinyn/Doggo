418f1fee0c0a5329c8570ce35ed1090d25dfda78
Shinyn <mattias.alm@hotmail.com>
2019-04-04 00:34:09 +0200

    MessageFragment is red..

--- app/src/main/java/com/l/doggo/HomeActivity.java
+++ app/src/main/java/com/l/doggo/HomeActivity.java
@@ -2,6 +2,7 @@ package com.l.doggo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -11,6 +12,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import android.app.Fragment;
+import android.widget.Switch;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -36,6 +40,13 @@ public class HomeActivity extends AppCompatActivity {
                     @Override
                     public boolean onNavigationItemSelected(MenuItem menuItem) {
                         menuItem.setChecked(true);
+                        switch (menuItem.getItemId()) {
+                            case R.id.nav_petsMenu:
+                                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MessageFragment()).commit();
+                        }
+
+
+
                         drawerLayout.closeDrawers();
 
                         return true;
