930d9231c92feb22beb9475b4fe4dfddeb8774f7
Shinyn <mattias.alm@hotmail.com>
2019-04-03 23:51:59 +0200

    Dog class and fragments

---
+++ app/src/main/java/com/l/doggo/Dog.java
@@ -0,0 +1,78 @@
+package com.l.doggo;
+
+public class Dog {
+
+    private String name;
+    private String breed;
+    private int age;
+    private int height;
+    private int weight;
+    private boolean neutered;
+    private boolean male;
+
+    public Dog(String name, String breed, int age, int height, int weight, boolean neutered, boolean male) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.neutered = neutered;
+        this.male = male;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public boolean isNeutered() {
+        return neutered;
+    }
+
+    public void setNeutered(boolean neutered) {
+        this.neutered = neutered;
+    }
+
+    public boolean isMale() {
+        return male;
+    }
+
+    public void setMale(boolean male) {
+        this.male = male;
+    }
+}
