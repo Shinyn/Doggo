21141adbcb09f04a389d1cb3d954a10ee17f623d
Shinyn <mattias.alm@hotmail.com>
2019-04-24 20:54:45 +0200

    Working on Uri

--- app/src/main/java/com/l/doggo/AddPetFragment.java
+++ app/src/main/java/com/l/doggo/AddPetFragment.java
@@ -1,6 +1,12 @@
 package com.l.doggo;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,20 +14,31 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 
@@ -35,12 +52,16 @@ public class AddPetFragment extends Fragment {
     private ArrayList<Dog> dogArrayList = new ArrayList<>();
     int number;
 
-    CollectionReference collectionRef;
-
-
     Button createDogBtn;
     RadioGroup radioGroup;
     Dog dog;
+
+    Button addDogImageBtn;
+    ImageView dogImage;
+    Uri imageUri;
+    private static final int PIC_IMAGE_REQUEST = 1;
+    StorageReference storageReference;
+    DatabaseReference databaseReference;
 
     @Nullable
     @Override
@@ -48,9 +69,62 @@ public class AddPetFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_add_pet, container, false);
     }
 
+    public void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PIC_IMAGE_REQUEST);
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver contentResolver = getContext().getContentResolver(); // Kanske är null
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private void uploadFile() {
+        if (imageUri != null) {
+            final String owner = FirebaseAuth.getInstance().getCurrentUser().getUid();
+            StorageReference fileReference = storageReference.child(owner);
+            fileReference.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    taskSnapshot.getMetadata().getReference().getDownloadUrl().toString();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PIC_IMAGE_REQUEST && resultCode == Activity.RESULT_OK
+        && data != null && data.getData() != null) {
+            imageUri = data.getData();
+            dogImage.setImageURI(imageUri);
+        }
+    }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        addDogImageBtn = getView().findViewById(R.id.add_pet_image_button);
+        dogImage = getView().findViewById(R.id.addDogPicture);
+
+        addDogImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+
 
         /*--------------------------------------------------------------*/
 
@@ -86,18 +160,20 @@ public class AddPetFragment extends Fragment {
         // Ta den inloggade användarens userID och sätt till owner, behöver inte hämta från firebase med get
         /*-------------------------------------------------*/
 
-
-
-        //----
+        storageReference = FirebaseStorage.getInstance().getReference("uploads");
+        databaseReference = FirebaseDatabase.getInstance().getReference("uploads");
+        // Lägg till dog image url här?
+        // Och istället för att img url är null så sätt den till imageUri
+        //---------------------------------------------------------------
 
 
         // Känns som extrajobb att ta en siffra -> konvertera till String -> konvertera till siffra
         // Bara för att få in den i Dog klassens konstruktor
-        final String name2 = name.getText().toString();
-        final String breed2 = breed.getText().toString();
-        final String age2 = age.getText().toString();
-        final String height2 = height.getText().toString();
-        final String weight2 = weight.getText().toString();
+        final String name2 = name.getText().toString().trim();
+        final String breed2 = breed.getText().toString().trim();
+        final String age2 = age.getText().toString().trim();
+        final String height2 = height.getText().toString().trim();
+        final String weight2 = weight.getText().toString().trim();
 
 
         // Kollar om hunden är kastrerad eller ej
@@ -112,8 +188,8 @@ public class AddPetFragment extends Fragment {
                 */
 
         // Kollar hane eller hona -> ändra med setGender till true eller false
-        radioGroup = getView().findViewById(R.id.radioGroup);      // <-- LÖSNINGEN!!!
-        final int radioId = radioGroup.getCheckedRadioButtonId();  // Detta värde VAR null här = crash
+        radioGroup = getView().findViewById(R.id.radioGroup);
+        final int radioId = radioGroup.getCheckedRadioButtonId();
         genderButton = getView().findViewById(radioId);
         if (radioId == R.id.radioBtnMale) {
             genderCheck = true;
@@ -123,7 +199,6 @@ public class AddPetFragment extends Fragment {
 
 
         final String owner = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        // Krashar här pga null
         DocumentReference docRef = FirebaseFirestore.getInstance().collection("users").document(owner);
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
@@ -135,18 +210,14 @@ public class AddPetFragment extends Fragment {
                 if (!name2.equals("") || !breed2.equals("") || !age2.equals("")
                         || !height2.equals("") || !weight2.equals("")) {
 
-                    //FirebaseFirestore db = FirebaseFirestore.getInstance();
-                    //FirebaseAuth auth = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                    // Skapar ny hund
 
+                    // Skapar ny hund
                     dog = new Dog(name2, breed2, owner, null, number,
                             Integer.parseInt(age2), Integer.parseInt(height2), Integer.parseInt(weight2),
                             neuteredCheck, genderCheck); /* Integer ger NumberFormatException om man inte gör try / catch */
 
-                    // Lägger till hunden till ArrayListan
+                    // Lägger till hunden till ArrayListan med toastmeddelande
                     addDogToArrayList(dog);
-
-
                     Toast.makeText(getActivity(), "Dog created", Toast.LENGTH_SHORT).show();
 
                     // clear all editTexts / set all paramaters to default
@@ -162,23 +233,19 @@ public class AddPetFragment extends Fragment {
                 } else {
                     Toast.makeText(getActivity(), "You have to fill all fields correctly", Toast.LENGTH_SHORT).show();
                 }
-
-
             }
         });
-
     }
 
     public void addDogToArrayList(Dog dog) {
+        // Uppdaterar listan på firestore
         dogArrayList.add(dog);
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        db.collection("dogs").add(dog);
+        //String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
         // Lägg till hund i database under /dogs/dogOwner/userDogs
         // Använd userId i dokumentet och skapa en collection som alla får läsa men bara userId får skriva till
-      //  db.collection("dogs").document(userId).collection("userDogs").add(dog);
-        db.collection("dogs").add(dog);
-
-        // Uppdatera listan på firestore
+        //db.collection("dogs").document(userId).collection("userDogs").add(dog);
     }
 
     public void printDogArrayList() {
